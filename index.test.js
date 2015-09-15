2cfaab3ba9b22b0353a320ffc97b1dded5eb5833
Jonas Cerqueira da Silva Junior <jonas.cerqueira@itigosystems.com.br>
2015-09-15 18:25:03 -0300

    Test Commit

--- index.test.js
+++
@@ -1,9 +0,0 @@
-describe('angular-input-masks-standalone', function () {
-	var moduleName = require('./index.js');
-
-	beforeEach(angular.mock.module('ui.utils.masks'));
-
-  it('should export the module name', function () {
-		expect(moduleName).toBe('ui.utils.masks');
-	});
-});
